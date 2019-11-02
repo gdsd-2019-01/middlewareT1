86c45f49310b057f00e242769a73bf35c03b503f
unknown <nikepick@gmail.com>
2019-11-02 17:53:45 +0530

    Updated the coverter for Middleware Task 2

---
+++ functions.js
@@ -0,0 +1,98 @@
+/////////////// CONVERTER //////////////////
+function convert(){
+    var c1 = document.getElementById("currency1").value;
+    var c2 = document.getElementById("currency2").value;
+    var name = document.getElementById("customer").value;
+    var result;
+    if(c2!=0 && c1!='') {
+        if (c2=='INR') {
+            c2 = 78.49;
+            result = 'Rs.';
+        } else if (c2== 'USD') {
+            c2 = 1.11;
+            result = '$';
+        }  else if (c2== 'BPS') {
+            c2 = 0.86;
+            result = '£';
+        } else {
+            c2 = 120.41;
+            result = '¥';
+        }
+        var amount = c1*c2;
+        result = result + amount.toFixed(2);
+        document.getElementById("result").value = result;
+        document.getElementById("ca").innerHTML = name + " your converted amount is";
+    }
+}
+
+//////////////// RESET VALUES ////////////////
+function reset(){
+    document.getElementById("currency1").value = '';
+    document.getElementById("customer").value = '';
+    document.getElementById("currency2").value = '0';
+    document.getElementById("result").value = '';
+    document.getElementById("ca").innerHTML = "Coverted Amount";
+}
+
+/////////////////////////// Database /////////////////////////
+
+////////////////// GET /////////////////////////
+function loadHistory(){
+
+    //////////SET LOG DIV HEIGHTEQUAL TO CONVERTER HEIGHT//////////
+    var elmnt = document.getElementById("convertWindow");
+    var height = elmnt.offsetHeight
+    document.getElementById("history").style.height = height;
+
+    //////////// AJAX REQUEST TO GET DATA ////////////////////
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200)
+        {
+            document.getElementById('history').innerHTML = '';
+            var result = this.responseText;
+
+            var results = JSON.parse(result);
+            var list = document.createElement("ul");
+            var i = 0; //made for getting number of conversions
+            results.forEach((history)=>
+        {
+            i++;
+            var listItem = document.createElement("li");
+            list.className = "list-group";
+            listItem.className = "list-group-item";
+            var data = document.createTextNode(history.name+" converted "+history._from+" euro to "+history._to);
+            listItem.appendChild(data);
+            list.appendChild(listItem);
+            document.getElementById('history').appendChild(list);
+        });
+            document.getElementById("counter").innerHTML = i + " Conversions Done";
+        }
+    }
+
+    xhttp.open("GET", "/home", true);
+    xhttp.send();
+}
+
+/////////////INSERTION////////////////
+function insert()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200)
+        {
+            loadHistory();
+        }
+    }
+
+    var name = document.getElementById('customer').value;
+    var _from = document.getElementById('currency1').value;
+    var _to = document.getElementById('result').value;
+
+    xhttp.open("POST", "/insert", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send('{"name":"'+name+'", "_from":"'+_from+'","_to":"'+_to+'"}');
+}
+
+
--- server.js
+++ server.js
@@ -1,17 +1,65 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const con = require('./db');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not Found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+const hostname = 'localhost'
+const port = '3000'
+
+function onRequest(req, res) {
+    if(req.method == 'GET' && req.url == '/')
+    {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream('./index.html').pipe(res);
+    }
+    else if(req.method == "GET" && req.url == '/functions.js')
+    {
+        res.writeHead(200, {"Content-Type":"text/javascript"});
+        fs.createReadStream("./functions.js").pipe(res);
+    }
+    else if(req.method == "GET" && req.url == '/home')
+    {
+        res.statusCode == 200;
+        res.setHeader('Content-Type', 'application/json');
+
+        var conn = con.getConnection();
+
+        conn.query('SELECT * FROM amounts ORDER BY `amounts`.`id` DESC', function(error, results, fields){
+            if(error) throw error;
+
+            var comments = JSON.stringify(results);
+
+            res.end(comments);
+        });
+
+        conn.end();
+    }
+    else if(req.method == "POST" && req.url == "/insert")
+    {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+
+        var content = '';
+        req.on('data', function(data){
+            content += data;
+
+            var obj = JSON.parse(content);
+            var conn = con.getConnection();
+
+            conn.query('INSERT INTO history.amounts (amounts.name, amounts._from, amounts._to) VALUES (?,?,?)',[obj.name,obj._from,obj._to], function(error, results, fields){
+            if(error) throw error;
+            console.log("Success!");
+        });
+
+        conn.end();
+        res.end("Success!");
+        });
+    }
+
 }
 
-http.createServer(onRequest).listen(8080);
+const server = http.createServer(onRequest);
+
+server.listen(port, hostname, () =>{
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
